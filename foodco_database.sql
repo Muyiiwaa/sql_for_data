1a9740556b7de479c9894dc5e8b99377d689825f
Muyiwa <forevermuyii@gmail.com>
2024-02-06 16:50:25 +0100

    foodco database creation

---
+++ foodco_database.sql
@@ -0,0 +1,52 @@
+-- creating database foodco
+
+create database foodco;
+
+use foodco;
+
+/*
+creating 3 tables namely
+branch, orders, staff
+i go start with branch because say 
+i no fit put my products fo roadside
+*/
+create table branch(
+	branch_id int,
+    street varchar(50) not null unique,
+    city varchar(25) not null,
+    
+    primary key(branch_id)
+);
+
+create table staff(
+	staff_id int,
+    fullname varchar(55) not null,
+    gender enum('m','f','binary'),
+    branch_id int not null,
+    
+    primary key(staff_id),
+    foreign key (branch_id) references branch (branch_id)
+);
+
+create table orders(
+	order_id int auto_increment,
+    amount decimal not null,
+    orderdate datetime default now(),
+    branch_id int not null,
+    staff_id int not null,
+    payment_type enum('cash','card','transfer') default 'cash',
+    
+    primary key(order_id),
+    foreign key (branch_id) references branch (branch_id),
+    foreign key (staff_id) references staff (staff_id)
+);
+
+-- insert data into all the tables in the database
+insert into branch(branch_id, street, city)
+values
+(1, 'bodija','Ibadan'),
+(2,'Tanke','Ilorin'),
+(3,'Ekosodin','Benin'),
+(4,'Lekki','Lagos');
+
+
