fb088f8ca3fd284efeaf82ba94d154d8ab6fe2ca
Muyiwa <forevermuyii@gmail.com>
2024-03-04 09:27:34 +0100

    timz's class

---
+++ timz_class.sql
@@ -0,0 +1,20 @@
+--  select and from clause --
+
+-- 1. return the names and unitprice of all our products
+select productname, unitprice
+from product;
+
+-- 2. return the firstname, lastname and country of our employees
+select firstname, lastname, country
+from employee;
+
+-- 3. return the names, city and country of our customers
+
+select companyname, contactname, city, country
+from customer;
+
+-- Exercise--
+-- 1. return the shipperid, companyname and number 
+-- of our shipping companies
+-- 2. return the birthdate, hiredate and managerid of our employees
+
